bac1ae6ad522cc028a0f228743d1295b7887f1ec
Logan McAnsh <logan@mcan.sh>
2017-09-29 16:55:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const printBadges = (names) => {
+  names.forEach((name, i) => {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  });
+  return names;
+};
+
+const tailsNeverFails = () => {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails += 1;
+  }
+
+  return `You got ${tails} tails in a row!`;
+};
