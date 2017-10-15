af95692910041ae36b26ce31a222798d4b965f45
Dominique McCloud <niquelejon@gmail.com>
2017-10-15 04:26:38 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const employees = ['Ada', 'Brendan', 'Ali']
+
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees
+}
+
+  printBadges(employees);
+
+
+  function tailsNeverFails () {
+    let tails = 0;
+
+    while (Math.random() >= 0.5) {
+      tails++;
+    }
+
+    return `You got ${tails} tails in a row!`;
+  }
