82e6bb52e1df53a1260d2453e1e12886f31da19c
Allen Anderson <dande313@byu.net>
2017-10-17 21:10:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function printBadges(name_array){
+  for (i = 0; i < name_array.length; i++){
+    console.log(`Welcome ${name_array[i]}! You are employee #${i + 1}.`);
+  };
+  return name_array;
+}
+
+function tailsNeverFails (){
+  let tails_count = 0;
+   while (Math.random() >= 0.5) {
+     tails_count++;
+   }
+  return `You got ${tails_count} tails in a row!`;
+}
