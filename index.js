0e7bf533c19440032a5394fee142a9a94174a799
¨anniezh12¨ <aniqaelahi@yahoo.com>
2017-11-04 14:01:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function printBadges(arr)
+{
+  for(let i = 0;i<arr.length; i++)
+  {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`);
+  }
+  return  arr;
+}
+
+function tailsNeverFails()
+
+{
+  let tails = 0;
+  let num = Math.floor(Math.random()*10+1)
+  while(num>= 0.5)
+    {tails++;}
+    return tails;
+  }
+
+
+}
