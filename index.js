4f6e8703b8465c007890ff5668307c289bf8f833
Joey Cho <cjy12182000@naver.com>
2017-11-05 19:25:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(array){
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
+
+function tailsNeverFails(){
+  tails = 0
+  while (maybeTrue()) {
+    tails++
+  }
+  return `You got ${tails} tails in a row!`
+  function maybeTrue(){
+    return Math.random() >= 0.5;
+  }
+}
