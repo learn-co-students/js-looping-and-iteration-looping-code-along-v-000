5454b78ca1504e08cfc209c81a0d1e64c6d41b09
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-11-05 10:36:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees
+};
+
+function tailsNeverFails () {
+
+  function heads () {
+    return Math.random() >= 0.5;
+  }
+
+  let x = 0;
+  while (heads()) {
+    x = x + 1;
+  }
+  return `You got ${x} tails in a row!`
+}
