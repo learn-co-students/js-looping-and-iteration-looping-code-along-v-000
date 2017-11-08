9dff143423a81f7c6473d31a281bcb9bfa8745b6
Ser Yoon Yun <sy1251@nyu.edu>
2017-11-08 03:52:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(arr){
+  for(var i = 0; i < arr.length; i++){
+    console.log("Welcome "+arr[i]+"! You are employee #"+(i+1)+".");
+  }
+  return arr;
+}
+
+function tailsNeverFails(){
+  let num = 0;
+  while(Math.random() >= 0.5){
+    num ++;
+  }
+  return "You got " + num + " tails in a row!";
+}
