57e7249f28bde872ef7d9b9fcfa6708fa114443f
Catherine Cordova <ccordova41@gmail.com>
2017-11-08 05:54:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${[i + 1 ]}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
+ // If the condition evaluates to true, the coin landed on 'Tails' and is flipped
+ // again. If the condition evaluates to false, the coin landed on 'Heads', and the
+ // 'Tails' streak is broken.
+ // At the end of the function, return a message to the user indicating how many
+ // times the coin landed on 'Tails' in a row, e.g.:
