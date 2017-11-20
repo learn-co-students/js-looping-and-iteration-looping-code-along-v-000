7858c5bc985ad6427a502da3a3449c5256fe177d
burrahey <burra.archana@gmail.com>
2017-11-19 22:10:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for(var i = 0; i < names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  count = 0
+
+    function maybeTrue(){
+      return Math.random() => 0.5;
+    }
+
+    while(maybeTrue()){
+      count++;
+    }
+  return count
+}
