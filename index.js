303af60b64d400fd45c25c42d8a03c0ede25b5c9
Alexandra Wright <superbiscuit@gmail.com>
2017-11-21 14:26:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(employee_array){
+    for (let index = 0; index < employee_array.length; index++) {
+        employee = employee_array[index]
+        console.log(`Welcome ${employee}! You are employee #${index + 1}.`)
+    }
+    return employee_array
+}
+
+function maybeHeads() {
+    return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+    let flips = 0
+    while (maybeHeads()) {
+        flips++
+    }
+    return `You got ${flips} tails in a row!`
+}
