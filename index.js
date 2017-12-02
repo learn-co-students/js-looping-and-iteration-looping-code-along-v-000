90ea45bf0f7f703e38f1b03bac95d90444be67b4
curlywallst <nnoyes@comcast.net>
2017-12-02 14:23:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (let i=0; i<arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`)
+  }
+  return arr
+}
+
+function tailsNeverFails() {
+  let numberTails = 0
+  while (Math.random()>=.5) {
+    numberTails++
+  }
+  return `You got ${numberTails} tails in a row!`
+}
