65b56151cbe8a666a043df848c478f973e5213b6
Sophia Varella <sophiavarella@gmail.com>
2017-12-11 18:00:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
+}
+
+
+// invokes Math.random to simulate coin flips
+// returns the number of 'Tail' flips in a row 
+function tailsNeverFails() {
+  let tailStreak = 0;
+
+  while (Math.random() >= 0.5) {
+    tailStreak ++;
+  }
+
+  return `You got ${tailStreak} tails in a row!`;
+}
