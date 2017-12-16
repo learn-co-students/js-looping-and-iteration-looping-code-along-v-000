8770c7d0d0575e1e5906fac3ec4400c60d9336aa
lpassamano <leighayna.josele@gmail.com>
2017-12-16 14:36:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i ++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails() {
+  function coinFlip() {
+    return Math.random() >= 0.5;
+  }
+
+  let tails = 0
+
+  while(coinFlip()) {
+    tails++
+  }
+  return `You got ${tails} tails in a row!`
+}
