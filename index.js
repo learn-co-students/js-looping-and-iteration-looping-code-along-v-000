4ef1061f3d43a8c6b0ca10117fa364fbcc8c0f02
kyleblee <hello.kylelee@gmail.com>
2017-12-29 18:27:57 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
+  return names;
+}
+
+function tailsNeverFails () {
+  let tailsCount = 0;
+  while (coinFlip()) {
+    tailsCount++;
+  }
+  return `You got ${tailsCount} tails in a row!`;
+}
+
+function coinFlip () {
+  return Math.random() >= 0.5;
+}
