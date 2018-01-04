e3da0a851a3fba36d92367220acf7b425d1f14df
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 15:54:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+names = ["Abel", "Ari", "Koi"]
+
+function printBadges(array) {
+  for (i = 0; i < array.length; i++) {
+    console.log("Welcome " + array[i] + "! You are employee #" + (i + 1) + ".")
+  }
+  return array
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+
+  while(Math.round(Math.random()) > 0) {
+    counter++;
+  }
+
+  return "You got " + counter + " tails in a row!"
+}
