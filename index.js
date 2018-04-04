21022a3b0473c9b5844ca8315691c99a49c73ab9
mbalsamo <github email address>
2018-04-04 10:04:10 -0400

    done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const employees = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges (employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails () {
+  let counter = 0;
+  while (maybeTrue()) {
+    ++counter;
+  }
+  return `You got ${counter} tails in a row!`;
+}
