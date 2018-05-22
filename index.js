1c68dd01fa7097d6b4d241025c548849f3bc3d45
MyMac <mymac@JCB.local>
2018-05-22 09:58:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(employees) {
+	for (let i = 0; i < employees.length; i++) {
+		console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+	}
+	return employees;
+}
+
+function tailsNeverFails() {
+	let i = 0
+	while (Math.round(Math.random()) === 1) {
+		i++;
+	}	
+	return `You got ${i} tails in a row!`;
+}
