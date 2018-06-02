3939b35104a9ad969770714e9ab142ff9643cf43
badlychadly <chadmontoya21@yahoo.com>
2018-06-02 19:30:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+function printBadges(names) {
+  for (let i = 1; i < names.length + 1; i++) {
+    console.log(`Welcome ${names[i - 1]}! You are employee #${i}.`);  
+  }
+  return names
+}
+
+function tailsNeverFails() {
+  let tails = 0
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`
+}
