d02f6a84aec100b4b5bf227c10f41005bc2e6e90
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-11 20:54:48 +0000

    Done.

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ function printBadges(names) {
     counter ++
   }
   return `You got ${counter} tails in a row!`
+}function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+  }
+  return names;
 }
+ function tailsNeverFails() {
+  let counter = 0;
+  while (Math.random() >= 0.5) {
+    counter ++
+  }
+  return `You got ${counter} tails in a row!`
+}
+
