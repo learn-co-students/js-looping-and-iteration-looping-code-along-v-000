f4bb125b53a975e2dae69d0ed84fb436a7ff8ff4
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 19:23:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${[i+1]}.`);
+  }
+  return names;
+}
+
+printBadges(names);
