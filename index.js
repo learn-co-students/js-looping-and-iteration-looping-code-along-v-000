5016935c8eba44d41dae235ffe0edf23ac740dea
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-14 23:31:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges (nameArray) {
+  for (let i = 0; i < nameArray.length; i++) {
+    console.log(`Welcome ${nameArray[i]}! You are employee #${i+1}.`);
+  }
+  return nameArray;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+	while (Math.random() >= 0.5) {
+    count++;
+  }
+  return (`You got ${count} tails in a row!`);
+}
