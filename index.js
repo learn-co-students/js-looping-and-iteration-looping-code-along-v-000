758a2026b25bd9c0e9527510964e9ba44558e9fd
NeedsAUsername <kevinchi118@gmail.com>
2018-09-18 00:08:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees;
+}
+
+function coinFlip() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0
+  while (coinFlip()) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
