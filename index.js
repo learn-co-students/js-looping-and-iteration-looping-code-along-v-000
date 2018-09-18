7fc73e8e721fcf07e22c836e370300827c1d3733
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-18 18:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGift(gift) {
+  console.log(`Wrapped ${gift} and added a bow!`);
+}
+
+wrapGift(gifts[0]);
+wrapGift(gifts[1]);
+wrapGift(gifts[2]);
+
+function printBadges(arr){ 
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+  }
+  return arr;
+}
+
+function maybeTrue() { 
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){ 
+  let tails = 0; 
+  
+  while (maybeTrue()) { 
+    tails++;
+  }
+  
+  return `You got ${tails} tails in a row!`; 
+}
