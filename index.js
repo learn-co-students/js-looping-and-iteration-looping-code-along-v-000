d0d647547167df172eac8cd3b0ec79d106cee298
Sara Stanton <sara.mpa13@gmail.com>
2018-10-04 21:23:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = []
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i ++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+  }
+  return names;
+}
+
+function flippedTails() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+   while (flippedTails()) {
+     ++counter;
+   }
+  return `You got ${counter} tails in a row!`;
+}
