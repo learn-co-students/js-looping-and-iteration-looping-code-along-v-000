0f048200dc8d79b0740423972dd47379dcb43c4a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-05 15:36:21 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
 
 function printBadges(array) {
   for(let i=0; i < array.length; i++) {
-    console.log(`Welcome ${array[i]}! You are employee #${i+1}`)
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
   }
   return array
+}
+
+
+function tailsNeverFails() {
+  let a = []
+  while(Math.random() >= 0.5) {
+    a.push(" ")
+  }
+  return `You got ${a.length} tails in a row!`
 }
