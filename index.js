659d488db00cc5229d2265168c199a0f5c4b4a6b
Carol Greene <carolgreene3@gmail.com>
2018-10-19 23:08:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
+const gifts = ['teddy bear', 'drone', 'doll'];
 
 function wrapGifts(gifts) {
   for (let i = 0; i < gifts.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
   }
-
   return gifts;
 }
-
 wrapGifts(gifts);
 
-
-
-function printBadges (names) {
+function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
-
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
   }
-
-  return names;
+  return names
 }
 
-
 function tailsNeverFails() {
-  let tails = 0
-
-  while (Math.random() >= 0.5) {
-    tails++;
+  let tails = 0;                      //set tails counter to zero
+  while (Math.random() >= 0.5) {      //set condition
+    tails++                           //increment tails
   }
-  return `You got ${tails} tails in a row!`;
-
+  return `You got ${tails} tails in a row!`  //return how many tails in a row
 }
