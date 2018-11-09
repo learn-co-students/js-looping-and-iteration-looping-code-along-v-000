0177b41111bb08e83038c09c5a07b24e881c0c2f
jokerdelta <jokerdelta@yahoo.com>
2018-11-08 19:24:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function printBadges(names){
+
+  for (let i = 0; i < names.length; i++){
+
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+  }
+
+  return names;
+
+}
+
+function tailsNeverFails(){
+  let flips = 0;
+  while (Math.random() >= 0.5){
+    ++flips;
+  };
+  return(`You got ${flips} tails in a row!`);
+}
