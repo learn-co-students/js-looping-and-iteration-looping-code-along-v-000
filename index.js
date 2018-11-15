eb1bea024049eefd5c370d250eac11030fefa911
Ryan Brady <ryanbrady369@gmail.com>
2018-11-15 02:20:19 +0000

    Done.

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function printBadges(list) {
   return list
 }
 
-// function tailsNeverFails() {
-//   let tails = 0
-//   let heads = 0
-//   while (heads == 0) {
-//     if(Math.random() < .5) {
-//       tails++
-//     } else {
-//       heads++
-//       return `You got ${tails} tails in a row!`
-//     }
-//   }
-// }
+function tailsNeverFails() {
+  let tails = 0
+  let heads = 0
+  while (heads == 0) {
+    if(Math.random() >= .5) {
+      tails++
+    } else {
+      heads++
+      return `You got ${tails} tails in a row!`
+    }
+  }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -34,59 +34,59 @@ describe('index.js', () => {
 		});
 	});
 
-	// describe('tailsNeverFails()', () => {         //expects Math.random, but expects specific returns from it? tests also loop infinitely/crash despite code working properly in other settings
-	// 	let spy;
-  //
-	// 	beforeEach(() => {
-	// 		spy = sinon.stub(Math, 'random');
-	// 	});
-  //
-	// 	afterEach(() => {
-	// 		spy.restore();
-	// 	});
-  //
-	// 	it('invokes Math.random() to simulate coin flips', () => {
-	// 		spy.returns(0.2);
-  //
-	// 		tailsNeverFails();
-  //
-	// 		expect(spy.calledOnce).to.be.true;
-	// 	});
-  //
-	// 	it('returns the number of "Tails" flips in a row', () => {
-	// 		spy
-	// 			.onFirstCall()
-	// 			.returns(0.7)
-	// 			.onSecondCall()
-	// 			.returns(0.5)
-	// 			.onThirdCall()
-	// 			.returns(0.1);
-  //
-	// 		expect(tailsNeverFails()).to.eq('You got 2 tails in a row!');
-	// 	});
-  //
-	// 	it('loops indefinitely until a flip results in "Heads"', () => {
-	// 		spy
-	// 			.onCall(0)
-	// 			.returns(0.7)
-	// 			.onCall(1)
-	// 			.returns(0.6)
-	// 			.onCall(2)
-	// 			.returns(0.5)
-	// 			.onCall(3)
-	// 			.returns(0.9)
-	// 			.onCall(4)
-	// 			.returns(0.8)
-	// 			.onCall(5)
-	// 			.returns(0.5)
-	// 			.onCall(6)
-	// 			.returns(0.7)
-	// 			.onCall(7)
-	// 			.returns(0.6)
-	// 			.onCall(8)
-	// 			.returns(0.4);
-  //
-	// 		expect(tailsNeverFails()).to.eq('You got 8 tails in a row!');
-	// 	});
-	// });
+	describe('tailsNeverFails()', () => {         //expects Math.random, but expects specific returns from it? tests also loop infinitely/crash despite code working properly in other settings
+		let spy;
+
+		beforeEach(() => {
+			spy = sinon.stub(Math, 'random');
+		});
+
+		afterEach(() => {
+			spy.restore();
+		});
+
+		it('invokes Math.random() to simulate coin flips', () => {
+			spy.returns(0.2);
+
+			tailsNeverFails();
+
+			expect(spy.calledOnce).to.be.true;
+		});
+
+		it('returns the number of "Tails" flips in a row', () => {
+			spy
+				.onFirstCall()
+				.returns(0.7)
+				.onSecondCall()
+				.returns(0.5)
+				.onThirdCall()
+				.returns(0.1);
+
+			expect(tailsNeverFails()).to.eq('You got 2 tails in a row!');
+		});
+
+		it('loops indefinitely until a flip results in "Heads"', () => {
+			spy
+				.onCall(0)
+				.returns(0.7)
+				.onCall(1)
+				.returns(0.6)
+				.onCall(2)
+				.returns(0.5)
+				.onCall(3)
+				.returns(0.9)
+				.onCall(4)
+				.returns(0.8)
+				.onCall(5)
+				.returns(0.5)
+				.onCall(6)
+				.returns(0.7)
+				.onCall(7)
+				.returns(0.6)
+				.onCall(8)
+				.returns(0.4);
+
+			expect(tailsNeverFails()).to.eq('You got 8 tails in a row!');
+		});
+	});
 });
