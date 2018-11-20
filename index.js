fc11f979889f0f0ba15ef791e19ba576703b5523
arianaberger <ariana.berger@gmail.com>
2018-11-20 17:27:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function printBadges(badges) {
+	for (let i = 0; i < badges.length; i++) {
+		console.log(`Welcome ${badges[i]}! You are employee #${i + 1}.`);
+	}
+	return badges;
+}
+
+function tailsNeverFails() {
+	let count = 0;
+	while (Math.random() >= 0.5) {
+		count++;
+	}
+	return `You got ${count} tails in a row!`;
+}
+
