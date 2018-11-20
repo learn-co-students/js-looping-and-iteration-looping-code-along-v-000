c9aedc99001561b8eb08bcc83dbc63d4ed8dc7f0
Bryan Hooper <bryan@lindsayfarm.net>
2018-11-20 16:00:17 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+//
+function tailsNeverFails() {
+  let counter = 0;
+  while (Math.random() >= 0.5) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
+
+function printBadges(emp) {
+  for (let i = 0; i < emp.length; i++) {
+    console.log(`Welcome ${emp[i]}! You are employee #${i + 1}.`);
+  }
+  return emp;
+}
