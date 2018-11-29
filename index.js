1adb4dfc5c832a0f8706b0494e3846af4e0c41ea
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-11-28 19:27:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function printBadges(badge) {
+  for (let i = 0; i < badge.length; i++) {
+    console.log(`Welcome ${badge[i]}! You are employee #${i + 1}.`);
+  }
+  return badge;
+}
+
+
+function flipACoin() {
+  return a >= 0.5 ? 'Tails' : 'Heads';
+}
+
+function tailsNeverFails() {
+  let x = 0;
+  while (flipACoin() === 'Tails') {
+    x++;
+  }
+  console.log(`You got ${x} tails in a row!`);
+}
