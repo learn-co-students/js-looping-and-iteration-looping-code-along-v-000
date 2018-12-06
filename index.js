98d89b68b2df18ba4742b37fd85f559c2cb945e2
epic0448 <samuelogundiran@Yahoo.com>
2018-12-05 20:31:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function printBadges(name){
+    for (let i = 0; i < name.length; i++ ) {
+        console.log(`Welcome ${name[i]}! You are employee #${i + 1}.`);
+        // LOG: Welcome Ada! You are employee #1.
+
+    }
+    return name;
+}
+
+function tailsNeverFails() {
+    var tail = 0;
+     while (Math.random() >= 0.5){
+       tail++;
+     }
+    return(`You got ${tail} tails in a row!`);
+}
+ 
