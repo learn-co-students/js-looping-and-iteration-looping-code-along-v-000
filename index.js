7fdbfabc2ca00489c9f5ae62bce1d34cf4ce8ffa
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-07 22:28:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // Code your solutions in this file
-function printBadges(arrayNames) {
-  for (let i = 0, i < arrayNames.length, i++) {
-    console.log("Welcome, ${arrayNames[i]}! You are employee # ${i + 1}.")
+function printBadges(array) {
+
+  for (let i = 0; i < array.length; i++) {
+  console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
   }
-  return arrayNames;
+
+  return array;
 }
 
+
 function tailsNeverFails() {
-  counter = 0
-  while(Math.random() >= 0.5) {
-    counter += 1;
-  }
-  console.log("You got ${counter} tails in a row!")
+  let i = 0;
+
+  while (Math.random() >= 0.5)
+    {
+    i++;
+    }
+
+  return `You got ${i} tails in a row!`;
 }
