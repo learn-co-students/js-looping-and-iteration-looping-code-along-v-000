a2d23fdba77ff8d56607029410894ee19652a6bf
Kayla Cross <kac288@georgetown.edu>
2018-12-29 09:52:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function printBadges(list){
+  for (let i = 1; i <= list.length; i++){
+    console.log(`Welcome ${list[i-1]}! You are employee #${i}.`)
+  }
+  return list;
+}
+
+function tailsNeverFails(){
+  let counter = 0;
+  while(Math.random() >= 0.5){
+    ++counter;
+  }
+  return `You got ${counter} tails in a row!`;
+}
