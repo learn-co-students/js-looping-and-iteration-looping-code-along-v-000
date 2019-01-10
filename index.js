9d8c63ea31d9d84241d89fd001c0924a8ea52902
chrissygonzalez <chrissygonzalez@gmail.com>
2019-01-10 08:28:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(array){
+    for (let i = 0; i < array.length; i++){
+        console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+    }
+    return array;
+}
+
+function tailsNeverFails(){
+    let flips = 0;
+    while(Math.random() >= 0.5){
+        flips++;
+    }
+    return `You got ${flips} tails in a row!`;
+}
