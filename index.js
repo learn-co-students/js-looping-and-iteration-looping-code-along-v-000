9eae0c5fa25153bf5abf403b752ea73534fa2a4e
RonSala <ron.sala@gmail.com>
2019-01-20 09:35:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+// const names;
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+      console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+      return names;
+  }
+
+function tailsNeverFails () {
+  while (Math.random() >= 0.5);
+}
