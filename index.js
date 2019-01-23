1f0fb78130e473ce7a6af503c84ba144bab0ebb4
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-23 14:29:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+     console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+   }
+  return names;
+}
+function tailsNeverFails() {
+  function tails() {
+    return Math.random() >= 0.5;
+  }
+  let i = 0;
+  // do {
+  //   i += 1;
+  //
+  // } while (tails())
+  while (tails()) {
+    i++;
+  }
+  return `You got ${i} tails in a row!`;
+}
