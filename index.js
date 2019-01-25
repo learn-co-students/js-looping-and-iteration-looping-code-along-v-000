1f65695fc4538429fc0c119ced9e9e53534f89c1
mstmari <michael.landrum.st.marie@gmail.com>
2019-01-25 15:09:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Code your solutions in this file
-function printBadges(array) {
- for (let i = 0; i < array.length; i++) {
-   console.log(`Welcome ${array[i]}! You are employee #${i+1}.`)
- }
- return array;
+function printBadges(name) {
+  for(let i = 0; i < name.length; i++){
+    console.log(`Welcome ${name[i]}! You are employee #${i+1}.`);
+  }
+  return name
 }
-
 
 function tailsNeverFails() {
   let tails = 0
 
-  while (Math.random() >= 0.5) {
+  while(Math.random() >= 0.5) {
     tails++;
   }
 
-  return `You got ${tails} tails in a row!`;
+  return `You got ${tails} tails in a row!`
 }
