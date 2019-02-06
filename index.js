6c3193a3b04f9b441e8e8d9b6638981fe6536c86
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-06 23:47:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(ary) {
+  for (let i = 0; i < ary.length; i++) {
+    console.log(`Welcome ${ary[i]}! You are employee #${i + 1}.`);
+  }
+  return ary;
+}
+
+function tailsNeverFails() {
+  let i = 0;
+  while (Math.random() >= 0.5) {
+    i++;
+  }
+  return(`You got ${i} tails in a row!`);
+}
