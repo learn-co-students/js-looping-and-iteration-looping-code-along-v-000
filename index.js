ea314ec1c0ef9ba96354644384ac7b1495c2691e
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-06 23:23:31 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function printBadges(array) {
   }
   return array;
 }
+
+function tailsNeverFails() {
+  let counter = 0;
+  while (Math.random() >= 0.5) {
+    counter++
+  }
+  return (`You got ${counter} tails in a row!`);
+}
