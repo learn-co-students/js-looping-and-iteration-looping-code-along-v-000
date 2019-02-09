6f98130294cd06fac2d5def7e9ab2afce2217d95
Christine Cua <cloudycua@gmail.com>
2019-02-09 16:17:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
+  }
+  return employees;
+}
+
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (maybeTrue()) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
