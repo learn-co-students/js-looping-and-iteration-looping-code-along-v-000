7d9d896bf74957dc2d680d1b3b29472c7c5485a6
Mohammed Noori <kmanoorib@gmail.com>
2019-02-22 22:59:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function printBadges(arr) {
+  for (let number = 0; number < arr.length; number++) {
+    console.log(`Welcome ${arr[number]}! You are employee #${number+1}.`);
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  // if Math.random() evaluates to less than 0.5, the coin landed on Heads
+  // if it evaluates to more than 0.5, then it is Tails
+  // each time it gets Tails, add 1 to Tails counter
+  // when it's not Tails anymore, end it with "You got NUMBER tails in a row!"
+  let tailsCount = 0;
+  
+  function headsOrTails() {
+    return Math.random() >= 0.5;
+  }
+  
+  while (headsOrTails()) {
+    tailsCount++;
+  }
+  const string = `You got ${tailsCount} tails in a row!`;
+  return string;
+}
