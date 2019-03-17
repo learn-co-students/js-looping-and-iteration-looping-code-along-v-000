fd456775a12e679430c8371fa548eecfafef4fd0
Brad Schrag <brad.schrag@gmail.com>
2019-03-17 16:56:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const test = 'hello'
+
+function printBadges(employees) {
+	for (i = 0; i < employees.length; i++) {
+		console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`)
+	}
+
+	return employees;
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails ++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
