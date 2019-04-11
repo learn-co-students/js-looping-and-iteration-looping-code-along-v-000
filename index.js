a470c00921fc2e6491117dca724ce2eab4746614
MDAM182 <mdam2345@gmail.com>
2019-04-11 03:10:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+var names = [];
+
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+  return names;
+}
+// printBadges(names);
+
+// if num > 0.5 its tails... coin flipped again
+// if num < 0.5 its heads... tails streak broken
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails ++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
