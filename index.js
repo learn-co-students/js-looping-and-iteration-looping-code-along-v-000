bd9689ea512be207f38312ca3326e3eaa78f3a98
George  Pianka <gpianka1@gmail.com>
2019-04-18 06:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function printBadges(employeeNames) {
+  for (let i = 0; i < employeeNames.length; i++) {
+    console.log(`Welcome ${employeeNames[i]}! You are employee #${i+1}.`);
+  }
+  return employeeNames;
+}
+
+ function tailsNeverFails() {
+  let tails = 0;
+  let heads = 0;
+  while(heads === 0) {
+    if (Math.random() >= 0.5) {
+      tails++;
+    }
+    else {
+      heads++;
+    }
+  }
+  return (`You got ${tails} tails in a row!`);
+}
+
+tailsNeverFails();
