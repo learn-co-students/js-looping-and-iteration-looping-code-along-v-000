447decc1b9af6dfc06f60adb04b3db798f21c93e
geoffrey woodcock <theatomproject@gmail.com>
2019-04-20 19:11:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const badges = ['Ada', 'Brendan', 'Ali'];
+ 
+function printBadges(badges) {
+    for (let i = 0; i < badges.length; i++) {
+        console.log(`Welcome ${badges[i]} You are employee #${[i]}`);
+    }
+ 
+    return badges;
+}
+ 
+printBadges(badges);
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(Math.random() >= 0.5) {
+      counter++;
+  }
+  
+  console.log("You got " + counter + " tails in a row!");
+}
