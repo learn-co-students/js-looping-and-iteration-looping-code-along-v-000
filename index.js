1f80adc70771bbc2d3262fbeea930637f24c3ee6
Guess <guess@Guesss-iMac.local>
2019-05-08 13:42:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function  writeCards(x) {
+  let j = []
+  for (let i = 0; i < x.length; i++) {
+    j.push( `Thank you, ${x[i]}, for the wonderful surprise gift!`)
+  }
+  return j
+}
+
+
+function countdown(x) {
+  for(var i = x; i >= 0; i--) {
+    console.log(i)
+  }
+}
