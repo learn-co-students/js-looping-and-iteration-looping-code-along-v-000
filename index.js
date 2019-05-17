84e507ed46f3ddf42471465b60cd90f003843793
Katie Moosman <moosman.katie@gmail.com>
2019-05-17 09:44:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let thankYouMessages = []
+    for (let i = 0; i < names.length; i++ ) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYouMessages
+}
+
+function countdown(number) {
+    while (number >= 0) {
+      console.log(number--);
+    }
+
+}
