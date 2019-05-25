4359378015af90460b5feb7bcff440fd7015a7d9
catherinism <catcreates13@gmail.com>
2019-05-25 19:46:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const results = [];
+
+function writeCards(stringNames, eventName) {
+  let i = 0
+  while (i < stringNames.length) {
+    results.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+    i++;
+  }
+   return results;
+}
+
+function countdown(i) {
+  for (let i = 10; i > -1; i -= 1) {
+    console.log(i);
+  }
+}
