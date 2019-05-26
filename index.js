f3e197f213e4c3cc178b532d09fb2b2b277ede00
lpzraf <lpz.raf@gmail.com>
2019-05-26 14:52:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  const newArr = []
+
+  for (let i = 0; i < names.length; i++) {
+
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return newArr
+}
+
+writeCards(names, event)
+
+
+function countdown(num) {
+
+  while (num > -1) {
+      console.log(num);
+      num--;
+  }
+}
