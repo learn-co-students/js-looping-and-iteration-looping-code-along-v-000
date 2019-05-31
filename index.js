a586bdb58c5f6962eddebee233f02f634caeeb09
sarawall1187 <sarawall1187@gmail.com>
2019-05-31 15:37:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+   let newNames = []
+    for (let i = 0; i < names.length; i++){
+       newNames.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newNames;
+}
+
+function countdown(num){
+        while (num >= 0) {
+            console.log(num);
+            num--;
+          }
+}
