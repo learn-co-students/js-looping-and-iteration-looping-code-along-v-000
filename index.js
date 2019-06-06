28a5b801ee1a1236140fc4df39ad6ef5929bacf8
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-05 22:39:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(guestName, event) {
+  const cards = [];
+  for (let i = 0; i < guestName.length; i++) {
+    cards.push(`Thank you, ${guestName[i]}, for the wonderful ${event} gift!`);
+  };
+  return cards;
+};
+
+function countdown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1;
+  };
+  console.log(number);
+};
