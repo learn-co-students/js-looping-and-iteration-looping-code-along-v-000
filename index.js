62fa186815186f97fe47d7717cdab4bb50a98239
Dan Hintz <dhintz@live.com>
2019-06-22 18:37:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+  let result = [];
+  for(let i=0; i<array.length; i++) {
+    result.push(`Thank you, ${array[i]}, for the wonderful ${event} gift.`);
+  }
+  return result;
+}
+
+function countdown(i) {
+  while(i>=0) {
+    console.log(i);
+    i--;
+  }
+}
