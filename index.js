de4f187f5a1fe777385d43fb80cd73c62ddfada9
Tom Cho <tc.thomascho@gmail.com>
2019-07-27 19:26:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  const responses = [];
+
+  for (let i = 0; i < names.length; i++) {
+    responses.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+
+  return responses;
+}
+
+function countdown(from) {
+  for (let i = from; i >= 0; i--) {
+    console.log(i);
+  }
+}
