5a68f3e5f40b798550ebcf8747ae296178427991
Heather <heather.miller114@gmail.com>
2019-08-07 13:40:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards (names, event) {
+    let thanks = [];
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+}
+
+function countdown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count--);
+    } 
+}
