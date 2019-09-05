86f38bb5cde6fef835beffddd37a8ce3c36826cb
Nate Dietz <nsdietz@gmail.com>
2019-09-04 21:48:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, giftType) {
+	const thankYouCardMessages = []
+	
+	for (let i = 0; i < name.length; i++) {
+		thankYouCardMessages.push(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`);
+	}
+
+	return thankYouCardMessages;
+}
+
+function countdown(number) {
+	while(number >= 0){
+		console.log(number--);
+	}
+}
