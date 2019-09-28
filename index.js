35b83af543f1a355edf16a1da3dab4b3d0ab97ab
Rebecca Tong <rtong@buffalo.edu>
2019-09-27 20:09:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const newNames = []
+function writeCards(names, event){
+
+  for (let i = 0; i < names.length; i++){
+newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+return newNames;
+}
+
+
+function countdown(counter){
+  while (counter > 0){
+    console.log(counter);
+    counter--
+  }
+    console.log(0);
+}
