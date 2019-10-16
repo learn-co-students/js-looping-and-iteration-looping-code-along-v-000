ebcfc9b86d7528a4795a761df0487dcc25a889aa
david-forer <david.j.forer@gmail.com>
2019-10-16 18:24:34 +0900

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards( arrayNames, birthday ) {
+    let thankYou = []
+    for ( let i = 0; i < arrayNames.length; i++ ) {
+        thankYou.push( `Thank you, ${arrayNames[i]}, for the wonderful ${birthday} gift!`)
+    }
+    return thankYou
+}   
+    function countdown (i) {
+        while (i>-1) {
+            console.log(i--);
+        }
+    }
