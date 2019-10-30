0313e8508d8006bdd0e24cc5be50c6456259b350
rgrier104 <rgrier104@gmail.com>
2019-10-29 15:37:07 -1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let array = [];
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
