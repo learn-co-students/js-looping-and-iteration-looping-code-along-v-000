a3e9c4d1e8439b95e51a1f5b8eaeb0ae8a3c1df1
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-11 21:01:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  var thankYouMessages = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  if (number > 0) {
+    console.log(number);
+    var newNumber = number - 1;
+    countDown(newNumber);
+  }
+  else if (number == 0) {
+    console.log(number);
+     }
+}
