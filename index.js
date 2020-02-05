24d75c1ccbe50b02a1a46d1f83cacd7daa2a47f3
ADogTheWarrior <coolfudge@gmail.com>
2020-02-05 10:33:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(nameArray, adj){
+  let i = 0;
+  let newArray = [];
+  while(i < nameArray.length){
+    newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${adj} gift!`)
+    i++;
+  }
+  return newArray;
+}
+
+function countDown(int){
+  for (let i = int; i >= 0; i--){
+    console.log(i);
+  }
+}
