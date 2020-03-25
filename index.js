431ac4bffdd354b99d91622848547ad2f0fe2e4e
inception88 <bljones36@gmail.com>
2020-03-24 20:04:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let answer = [];
+    for (let i = 0; i < array.length; i++) {
+        answer[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+      return answer;
+}
+
+function countDown(c) {
+    while (c > -1) {
+        console.log(c);
+        c--;
+    }
+}
