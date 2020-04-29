3b7ae2e4a7fd821f89aa81a4242d8fc94e314339
Liz P <parisielizabeth@gmail.com>
2020-04-29 00:31:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+const messages = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+  console.log(num--);
+  }
+}
