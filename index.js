dc9554b2fd7df9d11303811e39969a44b63460a5
Eva Navarro <emanao@me.com>
2020-06-13 02:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
+ function writeCards(ary,event) {
+   const msgCollection=[];
+   for (let i = 0; i < ary.length; i++) {
+     msgCollection.push(`Thank you, ${ary[i]}, for the wonderful ${event} gift!`)
+   }
+   return msgCollection;
+}
+
 function countDown(i){
   let counter = 0;
   while(counter>=0 && counter<=i){
-    console.log(i)
+    console.log(counter)
     counter++;
   }
 
