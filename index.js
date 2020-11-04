3e4f56a9fa63733b8a197eb1dc7b273d96c14252
wakenightRyu <bennyhudsonhung@gmail.com>
2020-11-03 23:33:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let cards = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(cards, surprise) {
+    let answer = [];
+    for (let i=0; i < cards.length; i++) {
+        answer.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+    }  
+return answer;
+}
+
+function countDown(n) {
+    let i=0;
+    while (n >= i) {
+        console.log(n - i); i++;
+    }
+
+}
