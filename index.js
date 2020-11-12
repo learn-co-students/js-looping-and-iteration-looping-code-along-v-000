5e372fe8bbea2d902c134b841f34d2743c05f7c6
Amil <github email address>
2020-11-12 08:50:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let names = ["Ada", "Brendan", "Ali"];
+let ocassion = ["Birtday"];
+
+function writeCards(names, ocassion) {
+  let newArray = []
+  for (let i = 0; i < names.length; i++) {
+
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`);
+    // console.log(`Thank you ${names[i]}, for the wonderful ${ocassion} gift!`);
+  }
+  return newArray;
+}
+
+writeCards(names, ocassion);
+
+
+let number = 10;
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
