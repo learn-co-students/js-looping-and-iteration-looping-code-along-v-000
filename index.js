2f9a6fed7f0eeae69a9395f0482ab1955b1c70ba
John Dust <d24dust@hotmail.com>
2021-01-08 18:32:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names) {
+  const cards = [];
+  for (let i=0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return cards
+}
+
+function countDown(num) {
+  for (let i=num; i >= 0; i--) {
+    console.log(i)
+  }
+}
