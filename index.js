591b1912859e13a96f01fbbdfe4c979c5bd8cae3
Andrew <andrewcaniglia@gmail.com>
2021-02-20 00:28:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, str) {
+    const cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${str} gift!`);
+  }
+    return cards;
+} 
+
+function countDown(num) {
+    while(num > -1) {
+     console.log(num);
+     num--;
+    }
+}
