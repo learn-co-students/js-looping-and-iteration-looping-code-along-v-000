a5d047f849128cca4959677f9c0c0ef603d6c456
github username <github email address>
2021-05-02 20:16:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ["Buffy", "Thompson", "Joann"];
+const event = "birthday";
+ 
+function writeCards(names, event) {
+  let thankYou = []
+  for (let i = 0; i < names.length; i++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+ 
+  return thankYou;
+}
+ 
+wrapGifts(gifts);
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+    console.log(i)
+    }
+}
