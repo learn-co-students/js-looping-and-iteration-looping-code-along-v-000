8764aa4cf5d6d9fa702e5fd4b191919f9507d50b
KimMead <kimmeadows8@gmail.com>
2021-05-28 19:27:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countdown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
